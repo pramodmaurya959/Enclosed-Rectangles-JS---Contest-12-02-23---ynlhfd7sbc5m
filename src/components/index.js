8f501b5c1ebd02cc793c36ed59cec8a29325f805
pramodmaurya959 <118468618+pramodmaurya959@users.noreply.github.com>
2023-02-12 13:49:43 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -24,15 +24,15 @@ function updateStructure(rec1,rec2){
 	          children: []
 	}
 	return 
-	{ top: '20px',
+	{       top: '20px',
 	        left: '20px',
 		width: '40px',
 		height: '60px',
 	          children: [{
-		   top: '30px',
-	        left: '30px',
-		width: '20px',
-		height: '30px',
+		   top: '10px',
+		   left: '10px',
+		   width: '20px',
+		   height: '30px',
 	          children: []
 		  }]
 	}
